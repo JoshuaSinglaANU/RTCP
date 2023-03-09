62a006a7659f5911a76a11b3841d8a24bec3818e
Joshua Singla <joshua.singla@p10f63gk3.penten.lan>
2023-03-09 15:34:39 +1100

    User's can add their username, password, + first/lastname to database

--- routes/createAccount.js
+++ routes/createAccount.js
@@ -14,6 +14,31 @@ sequelize.authenticate()
   .then(() => console.log('Database Connected'))
   .catch(err => console.log('Error: ', err))
 
+  // Create the model for the query
+const User = sequelize.define('user', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING
+  },
+  first_name: {
+    type: DataTypes.STRING
+  },
+  last_name: {
+    type: DataTypes.STRING
+  }
+}, {
+  tableName: 'user',
+  timestamps: false
+});
+
+// Test the DB Connection
+sequelize.authenticate()
+  .then(() => console.log('Database Connected'))
+  .catch(err => console.log('Error: ', err))
+
 
   /* GET users listing. */
 router.get('/', async function(req, res) {
@@ -23,6 +48,7 @@ router.get('/', async function(req, res) {
 
 // Once the login form is posted, run this
 router.post('/', async function (req, res) {
+    await User.create({username: req.body.username, password: req.body.password, first_name: req.body.firstName, last_name: req.body.surname});
     return;
 })
 
