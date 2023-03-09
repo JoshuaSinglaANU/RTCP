10446f666ae6ebab89dfb1e1b814f0d163831053
Joshua Singla <joshua.singla@p10f63gk3.penten.lan>
2023-03-09 16:15:09 +1100

    Users can now add all of their details

--- routes/createAccount.js
+++ routes/createAccount.js
@@ -18,7 +18,6 @@ sequelize.authenticate()
 const User = sequelize.define('user', {
   username: {
     type: DataTypes.STRING,
-    allowNull: false
   },
   password: {
     type: DataTypes.STRING
@@ -33,6 +32,36 @@ const User = sequelize.define('user', {
   tableName: 'user',
   timestamps: false
 });
+
+const UserAddress = sequelize.define('user_address', {
+    address: {
+      type: DataTypes.STRING,
+    },
+    city: {
+      type: DataTypes.STRING
+    },
+    country: {
+      type: DataTypes.STRING
+    },
+    mobile: {
+      type: DataTypes.INTEGER
+    }
+  }, {
+    tableName: 'user_address',
+    timestamps: false
+  });
+
+  const UserPayment = sequelize.define('user_payment', {
+    provider: {
+      type: DataTypes.STRING,
+    },
+    account_no: {
+      type: DataTypes.STRING
+    }
+  }, {
+    tableName: 'user_payment',
+    timestamps: false
+  });  
 
 // Test the DB Connection
 sequelize.authenticate()
@@ -49,6 +78,8 @@ router.get('/', async function(req, res) {
 // Once the login form is posted, run this
 router.post('/', async function (req, res) {
     await User.create({username: req.body.username, password: req.body.password, first_name: req.body.firstName, last_name: req.body.surname});
+    await UserAddress.create({address: req.body.address, city: req.body.city, country: req.body.country, mobile: req.body.mobile});
+    await UserPayment.create({provider: req.body.paymentProvider, account_no: req.body.accountNumber});
     return;
 })
 
